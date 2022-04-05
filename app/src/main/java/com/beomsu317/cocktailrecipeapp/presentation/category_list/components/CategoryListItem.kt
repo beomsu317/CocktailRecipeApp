541d4a2bf5fae610b2380a1a34b84ff58623c298
beomsulee <beomsu317@gmail.com>
2022-04-05 23:50:31 +0900

    Ingredient UI added

--- app/src/main/java/com/beomsu317/cocktailrecipeapp/presentation/category_list/components/CategoryListItem.kt
+++ app/src/main/java/com/beomsu317/cocktailrecipeapp/presentation/category_list/components/CategoryListItem.kt
@@ -1,8 +1,5 @@
 package com.beomsu317.cocktailrecipeapp.presentation.category_list.components
 
-import android.util.Log
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,13 +8,7 @@ import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 
 @Composable
 fun CategoryListItem(
