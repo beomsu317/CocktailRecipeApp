780fa53711e294ae7829024108d3bc4b7d8ecc1e
beomsulee <beomsu317@gmail.com>
2022-04-13 20:21:16 +0900

    update version 1.1.0

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -4,5 +4,5 @@ object ProjectConfig {
     const val minSdk = 21
     const val targetSdk = 31
     const val versionCode = 2
-    const val versionName = "1.1"
+    const val versionName = "1.1.0"
 }
